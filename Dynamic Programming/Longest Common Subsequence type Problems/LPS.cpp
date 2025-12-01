343fc840ca746ba78d7d9973161d4c3bd143accb
Shubhra-Awasthi <shubhraawasthi750@gmail.com>
2025-12-01 21:57:54 +0530

    LPS

---
+++ Dynamic Programming/Longest Common Subsequence type Problems/LPS.cpp
@@ -0,0 +1,23 @@
+// Longest Palindromic Subsequence
+int longestPalindromeSubsequence(string s)
+{
+    string s1 = s;
+    reverse(s1.begin(), s1.end());
+    return lcs(s, s1);
+}
+
+int lcs(string &s1, string &s2)
+{
+    int n = s1.size(), m = s2.size();
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    for (int i = n - 1; i >= 0; i--)
+        for (int j = m - 1; j >= 0; j--)
+            if (s1[i] == s2[j])
+                dp[i][j] = 1 + dp[i + 1][j + 1];
+            else
+                dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
+
+    return dp[0][0];
+}
